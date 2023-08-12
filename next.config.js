70638dd1bfbad2182d3340f85fa2ea62ccce8241
jasper200207 <jasper12394@gmail.com>
2023-08-12 16:54:20 +0900

    set deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
